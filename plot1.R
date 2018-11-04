d14cf27327323bdd751944391935fb40068c9211
TomGarshol <35583474+TomGarshol@users.noreply.github.com>
2018-11-04 22:19:51 +0100

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,13 +1,13 @@
-## This first line will likely take a few seconds. Be patient!
+
 if(!exists("NEI")){
   NEI <- readRDS("SCC_PM25.rds")
 }
 if(!exists("SCC")){
   SCC <- readRDS("Source_Classification_Code.rds")
 }
-# Have total emissions from PM2.5 decreased in the United States from 1999 to 2008? 
-# Using the base plotting system, make a plot showing the total PM2.5 emission from all sources 
-# for each of the years 1999, 2002, 2005, and 2008.
+ 
+# Make a plot showing the total PM2.5 emission from all sources 
+# for the years 1999, 2002, 2005, 2008.
 
 aggregatedTotalByYear <- aggregate(Emissions ~ year, NEI, sum)
 
